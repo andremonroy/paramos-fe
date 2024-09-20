1662ea1b1c4100bcace73e066c06b1aa79933903
“andre” <andrea.c2006@gmail.com>
2024-09-20 08:50:42 -0500

    makeup, imgs, textos, etc

--- javascript/scriptIndex.js
+++ javascript/scriptIndex.js
@@ -2,20 +2,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     let bannerImages = [
         {
-            "url": "../imagenes/banner/paramo3.jpg",
-            "nombre": "Banner 01",
-            "descripcion": "Paramo Sumapaz"
+            "url": "../imagenes/banner/Paramo1.jpg",
+            "nombre": "Innovación para la conservación",
+            "descripcion": "Nuestro compromiso es devolverle la vida a los ecosistemas más vulnerables, creando soluciones innovadoras para que esta regeneración sea medible, escalable y replicable."
         },
         {
-            "url": "../imagenes/banner/paramo4.jpg",
-            "nombre": "Banner 02",
-            "descripcion": "Paramo "
+            "url": "../imagenes/banner/paramoOceta.jpeg",
+            "nombre": "Paramo Ocetá",
+            "descripcion": "Con más de 65 km² a 3,800 metros de altura, el Páramo de Ocetá es una fuente de vida, capturando hasta el 85% del agua de lluvia y sustentando ecosistemas y comunidades enteras."
         },
         {
-            "url": "../imagenes/banner/paramo5.jpg",
-            "nombre": "Banner 03",
-            "descripcion": "Family"
-        },
+            "url": "../imagenes/banner/paramosumapaz4.jpeg",
+            "nombre": "Paramo Sumapaz",
+            "descripcion": "Con más de 200 km² a 3,500 metros de altura, el Páramo de Sumapaz es el más grande del mundo y una fuente vital de agua, abasteciendo a millones de personas en Colombia."
+        },/*
         {
             "url": "../imagenes/banner/carrusel4.webp",
             "nombre": "Banner 04",
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function() {
             "url": "../imagenes/banner/paramo5.jpg",
             "nombre": "Banner 05",
             "descripcion": "Luxury"
-        },
+        },*/
     ]
 
     // Creacion de variables 
